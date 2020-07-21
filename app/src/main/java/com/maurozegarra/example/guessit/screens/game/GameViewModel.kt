76a5137bb8940d4aec5d4f3103eba262ac74c51c
Mauro Zegarra <mauro.zegarra@gmail.com>
2020-07-21 09:09:59 -0500

    Add LiveData

--- app/src/main/java/com/maurozegarra/example/guessit/screens/game/GameViewModel.kt
+++ app/src/main/java/com/maurozegarra/example/guessit/screens/game/GameViewModel.kt
@@ -2,22 +2,29 @@ package com.maurozegarra.example.guessit.screens.game
 
 import android.content.ContentValues.TAG
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
     // The current word
-    var word = ""
+    private val _word = MutableLiveData<String>()
+    val word: LiveData<String>
+        get() = _word
+
 
     // The current score
-    var score = 0
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
     init {
-        Log.i(TAG, "Called: GameViewModel created!")
         resetList()
         nextWord()
+        _score.value = 0
     }
 
     override fun onCleared() {
@@ -63,17 +70,17 @@ class GameViewModel : ViewModel() {
         if (wordList.isEmpty()) {
             //gameFinished()
         } else {
-            word = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
     }
 
     fun onSkip() {
-        score--
+        _score.value = (_score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score++
+        _score.value = (_score.value)?.plus(1)
         nextWord()
     }
 }
